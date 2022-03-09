365da3a51769cc3c14e49114355d5813c9597356
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-09 05:28:33 -0300

    Exercício 37

---
+++ Exercicio37.py
@@ -0,0 +1,6 @@
+#Faça um programa que peça dois números inteiros. Imprima a soma desses números na tela
+
+a = float(input('Entre com um número: '))
+b = float(input('Entre com o segundo  '))
+c = a + b
+print(c)
